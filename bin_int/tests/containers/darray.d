fabe09282057a86c854f885e47a130e08c6920f4
GYUVARAJU <gyuvaraj140603@gmail.com>
2024-10-27 22:51:11 +0530

    two

--- bin_int/tests/containers/darray.d
+++
@@ -1,4 +0,0 @@
-bin_int/tests/containers/darray.o: \
-  C:\Program\ Files\LLVM\lib\clang\18\share\asan_ignorelist.txt \
-  containers\darray.c containers\darray.h common\defines.h \
-  common\common.h
--- bin_int/tests/tests/main.d
+++
@@ -1,5 +0,0 @@
-bin_int/tests/tests/main.o: \
-  C:\Program\ Files\LLVM\lib\clang\18\share\asan_ignorelist.txt \
-  tests\main.c tests\common\test_manager.h common\defines.h \
-  tests\containers\test_darray.h tests\containers\test_list.h \
-  common\common.h
